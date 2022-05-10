9f061ef5ece9828b714764eeb94835b8e1c1ab02
Nur İlayda Mert <57589311+ilaydamert@users.noreply.github.com>
2022-05-10 14:21:57 +0300

    Update opengl.cpp

--- opengl.cpp
+++ opengl.cpp
@@ -1,19 +1,19 @@
 #include <GL/glut.h>
 #include <stdlib.h>
-void ayarlar(void)
+void settings(void)
 {
 glClearColor(0.0,0.0,0.0,0.0);
 glShadeModel(GL_FLAT);
 glOrtho(-2.0, 2.0, -2.0, 2.0, -1.0, 1.0);
 }
-void sekil(void)
+void shape(void)
 {
-glClear(GL_COLOR_BUFFER_BIT); //Çalýþmadan önce eskiden kalan pikselleri temizler
-glBegin(GL_TRIANGLE_STRIP); //Birbirine baðlý üçgenler oluþturmak için kullanýlýr.
-glVertex2f(0.0f,0.9f); //koordinatlar. 2 parametre alacaðýný ve float olduðunu temsil eder.
-glVertex2f(0.4f,0.1f); //ortak köþe noktasý
-glColor3f(1.0, 0.0, 0.55); //Renkler. 3 parametre alacaðýný ve float olduðunu temsil eder.
-glVertex2f(-0.4f,0.1f); //ortak köþe noktasý
+glClear(GL_COLOR_BUFFER_BIT); //Cleans old pixels before running
+glBegin(GL_TRIANGLE_STRIP); //It is used to form interconnected triangles.
+glVertex2f(0.0f,0.9f); //coordinates. Represents that it will take 2 parameters and is a float.
+glVertex2f(0.4f,0.1f); //common corner point
+glColor3f(1.0, 0.0, 0.55); //Colors. Represents that it will take 3 parameters and is a float.
+glVertex2f(-0.4f,0.1f); //common corner point
 glColor3f(1.0, 0.5, 0.5);
 glVertex2f(-0.0f,-0.7f);
 glEnd();
@@ -25,8 +25,8 @@ glutInit(&argc,argv);
 glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB );
 glutInitWindowPosition(0,0);
 glutInitWindowSize(200,200);
-glutCreateWindow("odev");
-ayarlar();
-glutDisplayFunc(sekil);
+glutCreateWindow("opengl");
+settings();
+glutDisplayFunc(shape);
 glutMainLoop();
 return 0; }
